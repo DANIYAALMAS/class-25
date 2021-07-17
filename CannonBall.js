b51dfd602a049a1a6aa81c6678c751c76579aef5
DANIYAALMAS <85950995+DANIYAALMAS@users.noreply.github.com>
2021-07-17 10:05:47 -0700

    Update CannonBall.js

--- CannonBall.js
+++ CannonBall.js
@@ -30,7 +30,7 @@ class CannonBall{
      image(this.image,0,0,this.r,this.r)
      pop()
      if(this.body.velocity.x>0 && this .body.position.x>300){
-       var position=[this.body.position.x,his.body.position.y]
+       var position=[this.body.position.x,this.body.position.y]
        this.trajectory.push(position)
      }
      for (var i = 0; i < this.trajectory.length; i++)
